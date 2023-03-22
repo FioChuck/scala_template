8d015156e50177c0689f008e076d383dfdfbee68
FiocChuck <chasf@google.com>
2023-03-22 11:35:41 -0400

    dependency mlib

--- build.sbt
+++ build.sbt
@@ -8,6 +8,7 @@ val sparkVersion = "3.1.3"
 
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % sparkVersion % "provided",
+  "org.apache.spark" %% "spark-mllib" % "3.1.3" % "provided",
   "org.apache.spark" %% "spark-sql" % sparkVersion % "provided",
   "com.google.cloud.bigdataoss" % "gcs-connector" % "hadoop3-2.2.6" % "provided",
   "com.google.cloud.spark" %% "spark-bigquery-with-dependencies" % "0.27.1"
--- src/main/scala/BqDemo.scala
+++ src/main/scala/BqDemo.scala
@@ -6,6 +6,7 @@ import org.apache.spark.sql.types.IntegerType
 import org.apache.spark.sql.types.StringType
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.functions._
+import org.apache.spark.ml.linalg
 
 object BqDemo {
   def main(args: Array[String]): Unit = {
@@ -13,31 +14,31 @@ object BqDemo {
     // TODO how to pass arguments in debug? Not possible with Metals?
     val spark = SparkSession.builder
       .appName("Bq Demo")
-      // .config("spark.master", "local[*]") // local dev
-      // .config(
-      //   "spark.hadoop.fs.AbstractFileSystem.gs.impl",
-      //   "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS"
-      // )
-      // .config("spark.hadoop.fs.gs.project.id", "cf-data-analytics")
-      // .config("spark.hadoop.google.cloud.auth.service.account.enable", "true")
-      // .config(
-      //   "spark.hadoop.google.cloud.auth.service.account.json.keyfile",
-      //   "/Users/chasf/Desktop/cf-data-analytics-f77cb40a9dbd.json"
-      // )
+      .config("spark.master", "local[*]") // local dev
+      .config(
+        "spark.hadoop.fs.AbstractFileSystem.gs.impl",
+        "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS"
+      )
+      .config("spark.hadoop.fs.gs.project.id", "cf-data-analytics")
+      .config("spark.hadoop.google.cloud.auth.service.account.enable", "true")
+      .config(
+        "spark.hadoop.google.cloud.auth.service.account.json.keyfile",
+        "/Users/chasf/Desktop/cf-data-analytics-f77cb40a9dbd.json"
+      )
       .getOrCreate()
 
-    // val df =
-    //   spark.read
-    //     .format("bigquery")
-    //     .option(
-    //       "table",
-    //       "cf-data-analytics.market_data.googl_window_v"
-    //     )
-    //     .option("viewsEnabled", "true")
-    //     .load()
+    val df =
+      spark.read
+        .format("bigquery")
+        .option(
+          "table",
+          "cf-data-analytics.zone_1.googl_market_data"
+        )
+        // .option("viewsEnabled", "true")
+        .load()
 
-    val df = spark.read
-      .parquet("gs://cf-spark-external/googl-market-data")
+    // val df = spark.read
+    //   .parquet("gs://cf-spark-external/googl-market-data")
 
     df.show()
 
@@ -53,10 +54,10 @@ object BqDemo {
         "temporaryGcsBucket",
         "cf-spark-temp"
       ) // indirect mode destination gcs bucket
-      .option("writeMethod", "indirect")
+      .option("writeMethod", "direct")
       .mode("overwrite") // overwrite or append to destination table
       .save(
-        "cf-data-analytics.market_data.googl_spark_ingestion_2"
+        "cf-data-analytics.market_data.googl_spark_ingestion_5"
       ) // define destination table
   }
 }
