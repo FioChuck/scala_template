202a2e4dcd989e8dc9d371f45e40bdd912feb843
FiocChuck <chasf@google.com>
2023-03-24 11:12:57 -0400

    destination update

--- src/main/scala/BqDemo.scala
+++ src/main/scala/BqDemo.scala
@@ -13,17 +13,17 @@ object BqDemo {
     // TODO how to pass arguments in debug? Not possible with Metals?
     val spark = SparkSession.builder
       .appName("Bq Demo")
-      // .config("spark.master", "local[*]") // local dev
-      // .config(
-      //   "spark.hadoop.fs.AbstractFileSystem.gs.impl",
-      //   "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS"
-      // )
-      // .config("spark.hadoop.fs.gs.project.id", "cf-data-analytics")
-      // .config("spark.hadoop.google.cloud.auth.service.account.enable", "true")
-      // .config(
-      //   "spark.hadoop.google.cloud.auth.service.account.json.keyfile",
-      //   "/Users/chasf/Desktop/cf-data-analytics-f77cb40a9dbd.json"
-      // )
+      .config("spark.master", "local[*]") // local dev
+      .config(
+        "spark.hadoop.fs.AbstractFileSystem.gs.impl",
+        "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS"
+      )
+      .config("spark.hadoop.fs.gs.project.id", "cf-data-analytics")
+      .config("spark.hadoop.google.cloud.auth.service.account.enable", "true")
+      .config(
+        "spark.hadoop.google.cloud.auth.service.account.json.keyfile",
+        "/Users/chasf/Desktop/cf-data-analytics-f77cb40a9dbd.json"
+      )
       .getOrCreate()
 
     val df =
@@ -31,10 +31,30 @@ object BqDemo {
         .format("bigquery") // added as dependency via sbt build
         .option(
           "table",
-          "cf-data-analytics.zone_1.googl_market_data"
+          "cf-data-analytics.raw_zone.googl_data"
         )
-        // .option("viewsEnabled", "true")
         .load()
+
+    val df2 = df.groupBy("dt").agg(max("trade_price")).orderBy("dt")
+    // df.withColumn("test", to_date(col("dt"), "MM-dd-yyyy")).show()
+    df2.show()
+    print("end")
+
+    // val df =
+    //   spark.read
+    //     .format("bigquery") // added as dependency via sbt build
+    //     .option(
+    //       "table",
+    //       "cf-data-analytics.market_data.googl_latest_trade_v"
+    //     )
+    //     .option("viewsEnabled", "true")
+    //     .load()
+
+    // val df2 = df.repartition(16).drop("trade_condition") // create 8 partitions
+
+    // df2.show()
+    // df2.printSchema()
+    // df2.write.format("parquet").save("gs://analytics-data-lake/googl-data")
 
     // df.show()
 
@@ -42,7 +62,7 @@ object BqDemo {
 
     // df.printSchema()
 
-    val df2 = df.repartition(16) // create 8 partitions
+    // val df2 = df.repartition(16) // create 8 partitions
 
     df2.write
       .format("bigquery")
@@ -53,7 +73,7 @@ object BqDemo {
       .option("writeMethod", "direct")
       .mode("overwrite") // overwrite or append to destination table
       .save(
-        "cf-data-analytics.market_data.googl_spark_ingestion_10"
+        "cf-data-analytics.composer_destination.googl_spark_summarized"
       ) // define destination table
   }
 }
